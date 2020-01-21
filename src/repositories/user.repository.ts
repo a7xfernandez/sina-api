094baa2cc5c482d580b7d336f6d57abee2165450
Jorge <00jfernandez@gmail.com>
2020-01-20 21:21:47 -0600

    Agregando funcionalidad para manejo de usuarios

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,13 +1,13 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {User, UserRelations} from '../models';
-import {SinaDbDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import { DefaultCrudRepository } from '@loopback/repository';
+import { User, UserRelations } from '../models';
+import { SinaDbDataSource } from '../datasources';
+import { inject } from '@loopback/core';
 
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.userName,
   UserRelations
-> {
+  > {
   constructor(
     @inject('datasources.sinaDb') dataSource: SinaDbDataSource,
   ) {
